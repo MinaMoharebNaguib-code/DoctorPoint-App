b4b3a3d1c074a256e0035be63e255073b7fddf0a
MinaMoharebNaguib-code <78856135+MinaMoharebNaguib-code@users.noreply.github.com>
2022-07-08 02:52:08 +0200

    Add files via upload

---
+++ Models/CountryCode.swift
@@ -0,0 +1,15 @@
+//
+//  CountryCode.swift
+//  DoctorPoint
+//
+//  Created by Mina Mohareb on 25/05/2022.
+//
+
+import Foundation
+
+struct CountryCode : Codable
+{
+    let name: String
+    let dial_code: String
+    let code: String
+}
