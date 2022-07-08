f45ceb9f0027a5ca2fef03ce49b8b419fa79f9ae
MinaMoharebNaguib-code <78856135+MinaMoharebNaguib-code@users.noreply.github.com>
2022-07-08 02:51:55 +0200

    Add files via upload

---
+++ Extensions/Constraints.swift
@@ -0,0 +1,26 @@
+//
+//  Constraints.swift
+//  DoctorPoint
+//
+//  Created by Mina Mohareb on 15/05/2022.
+//
+
+import Foundation
+
+enum TypeSetting{
+    case Pro, N, Language, Invite, Favourite, FAQs, Help
+}
+
+enum TypeFee
+{
+    case Voice, Messaging, Video
+}
+
+enum TypeDone
+{
+    case Voice, Messaging, Video, Mail
+}
+
+enum TypeNotification{
+    case Alarm, Notification
+}
